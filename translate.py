d1f7bdecfaa3d4f60e0f060458475370c470f549
Jack <drummingjack2@gmail.com>
2018-05-08 17:48:12 -0700

    Optimization for translate.py

--- translate.py
+++ translate.py
@@ -1,29 +1,14 @@
 from googletrans import Translator
 import re
 
-# Definitely shouldn't repeat the translate block 4 times, could condense with a function
+regex = r"(?<=text=)(.*)(?=, pronunciation)"
 
 
 def translate(string, lang1, lang2, lang3):
+    language_list = ['en', lang1, lang2, lang3, 'en']
     translator = Translator()
-    trans_string = str(translator.translate(string, lang1, 'en'))  # First translation, english to lang1
-    regex = r"(?<=text=)(.*)(?=, pronunciation)"  # Looks only for text between "text=" and ", pronunciation"
-    matches = re.search(regex, trans_string)  # Matches the regex pattern to the output of googleTrans
-    cleaned_translation = matches.group(0)  # String with just the translated text
-
-    trans_string = str(translator.translate(cleaned_translation, lang2, lang1))  # Second trans, lang1 > lang2
-    regex = r"(?<=text=)(.*)(?=, pronunciation)"
-    matches = re.search(regex, trans_string)
-    cleaned_translation = matches.group(0)
-
-    trans_string = str(translator.translate(cleaned_translation, lang3, lang2))  # Third trans, lang2 > lang3
-    regex = r"(?<=text=)(.*)(?=, pronunciation)"
-    matches = re.search(regex, trans_string)
-    cleaned_translation = matches.group(0)
-
-    trans_string = str(translator.translate(cleaned_translation, 'en', lang3))  # Second trans, lang3 > english
-    regex = r"(?<=text=)(.*)(?=, pronunciation)"
-    matches = re.search(regex, trans_string)
-    cleaned_translation = matches.group(0)
-
+    cleaned_translation = string
+    for i in range(4):
+        trans_string = str(translator.translate(cleaned_translation, language_list[i + 1], language_list[i]))
+        cleaned_translation = (re.search(regex, trans_string)).group(0)
     return cleaned_translation
